0bd38632becb570f88be5c77e5c227876c70ade0
@ivan-aksenov <rivenmain098@gmail.com>
2023-10-09 10:08:16 +0800

    feat: add pic

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,29 +1,30 @@
 <template>
     <div>
-        <nav class="bg-gray-900 flex justify-between items-center p-4">
-    <a href="#" class="text-white font-bold">My App</a>
-    <ul class="flex space-x-5 mx-auto">
-      <li>
-        <nuxt-link to="/" class="text-white hover:underline">WHAT WE DO?</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/feature" class="text-white hover:underline">FEATURE</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/insight" class="text-white hover:underline">INSIGHTS</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/about" class="text-white hover:underline">ABOUT US</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/contact" class="text-white hover:underline">CONTACT</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/about" class="text-white hover:underline">SIGN IN</nuxt-link>
-      </li>
-    </ul>
-  </nav>
+      <nav class="bg-slate-800 flex justify-between items-center p-4">
+        <a href="#" class="text-white font-bold">My App</a>
+        <ul class="flex space-x-5 mx-auto">
+          <li>
+            <nuxt-link to="/" class="text-white hover:underline">WHAT WE DO?</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/feature" class="text-white hover:underline">FEATURE</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/insight" class="text-white hover:underline">INSIGHTS</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/about" class="text-white hover:underline">ABOUT US</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/contact" class="text-white hover:underline">CONTACT</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/about" class="text-white hover:underline">SIGN IN</nuxt-link>
+          </li>
+        </ul>
+      </nav>
     </div>
+
 </template>
 
 <script setup>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <template>
     <navbar></navbar>
-    <div>
-        <h2 class="text-red-500">WHAT WE DO</h2>
+    <div class="flex justify-center">
+        <h2 class="text-center mt-10 border-white border-b-4 m-10">WHAT WE DO</h2>
     </div>
+
+    <div class="flex justify-center mt-10 w-[100%]">
+        <img src="~/assets/Rectangle 89.svg" >
+    </div>
+    
+
+    
 </template>
 
 <script setup>
@@ -10,6 +17,7 @@
 </script>   
 
 <style scoped>
+
     h2{
         margin-bottom: 20px;
         font-size: 36px;
